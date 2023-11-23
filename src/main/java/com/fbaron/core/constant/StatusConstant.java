7f882be8768d3e76fa4480fa1f3792cb73efd386
Ferney Estupinan Baron <festupinan@celerix.com>
2023-11-22 21:41:11 -0500

    Added API docs to SignUp rest endpoint

---
+++ src/main/java/com/fbaron/core/constant/StatusConstant.java
@@ -0,0 +1,18 @@
+package com.fbaron.core.constant;
+
+/**
+ * @author Ferney Estupinan Baron
+ * @version 0.0.1
+ * @since 11/22/2023
+ */
+public final class StatusConstant {
+
+    public static final String SUCCESS = "Success";
+    public static final String CREATED = "Created";
+    public static final String BAD_REQUEST = "Bad request";
+    public static final String INTERNAL_SERVER_ERROR = "Internal server error";
+
+    private StatusConstant() {
+    }
+
+}
--- src/main/java/com/fbaron/web/rest/SignUpRestAdapter.java
+++ src/main/java/com/fbaron/web/rest/SignUpRestAdapter.java
@@ -4,6 +4,11 @@ import com.fbaron.core.service.user.UserService;
 import com.fbaron.web.dto.user.SignUpDTO;
 import com.fbaron.web.dto.user.UserDTO;
 import com.fbaron.web.mapper.UserDtoMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static com.fbaron.core.constant.StatusConstant.*;
 
 /**
  * @author Ferney Estupinan Baron
@@ -30,6 +37,17 @@ public class SignUpRestAdapter {
     private final UserService userService;
     private final UserDtoMapper userDtoMapper;
 
+    @Operation(summary = "signUp",
+            description = "Registers a new user in the system by giving the RequestBody with its details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = CREATED,
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "400", description = BAD_REQUEST,
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", description = INTERNAL_SERVER_ERROR,
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTO> signUp(@RequestBody @Valid SignUpDTO dto) {
         log.debug("Received a request for POST /api/v1/sign-up EndPoint with RequestBody: {}", dto);
